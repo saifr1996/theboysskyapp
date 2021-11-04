080fc6b299aab1cb8a44941b344f9cdef447b7d7
miguel8998 <miguel8998@hotmail.com>
2021-11-04 16:32:21 +0000

    logic for the names

--- app.py
+++ app.py
@@ -5,16 +5,28 @@ app = Flask(__name__)
 @app.route("/", methods = ['POST', 'GET'])
 def main_page():
     rating = list(range(0,9))
-
     form_data = request.form
 
-    if request.method == 'POST':
-        name = request.form['customer_name']
-        engineer = request.form['subject']
-        return redirect(url_for('thankyou', name=name))
+    if request.args.get('first_time') == "0":
 
-  
-    return render_template("first_page.html", rating = rating, form_data = form_data)
+        print('else')
+        if request.method == 'POST':
+            if request.form['customer_name'] != "":
+                name = request.form['customer_name']
+                return redirect(url_for('thankyou', name=name))
+            else:
+                return redirect(url_for('main_page', first_time=0))
+        return render_template("first_page_alt.html")
+
+    else:
+        if request.method == 'POST':
+            name = request.form['customer_name']
+            if name != "":
+                return redirect(url_for('thankyou', name=name))
+            else:
+                return redirect(url_for('main_page', first_time=0))
+        return render_template("first_page.html", rating = rating, form_data = form_data)
+
 
 
 @app.route("/thankyou")
