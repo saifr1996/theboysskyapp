67d69fa723a37e102efe215fb5386dc7af2a0a94
Kevin Tao <kevintao841@yahoo.co.uk>
2021-11-04 12:00:28 +0000

    with forms and radio button

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def main_page():
+    rating = list(range(0,9))
     
-    return render_template("base.html")
+    form_data = request.form
+    #return render_template('data.html',form_data = form_data)
+    return render_template("first_page.html", rating = rating, form_data = form_data)
 
 
 @app.route("/test/")
