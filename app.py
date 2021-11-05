0a9a1cf16037fb710ff2d6c96b9b19956dfc4d14
Kevin Tao <kevintao841@yahoo.co.uk>
2021-11-05 10:12:51 +0000

    change empty name to valid name

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+import re
 
 app = Flask(__name__)
+
+#bool(re.fullmatch('[A-Za-z]{2,25}( [A-Za-z]{2,25})?', x)
+#this is the regex for full name
 
 @app.route("/", methods = ['POST', 'GET'])
 def main_page():
@@ -8,20 +12,20 @@ def main_page():
     form_data = request.form
 
     if request.args.get('bad_input') == "0":
-
-        print('else')
         if request.method == 'POST':
             if request.form['customer_name'] != "":
                 name = request.form['customer_name']
                 return redirect(url_for('thankyou', name=name))
             else:
                 return redirect(url_for('main_page', bad_input=0))
+        # this will never be reached
         return render_template("first_page_alt.html")
 
     else:
         if request.method == 'POST':
             name = request.form['customer_name']
-            if name != "":
+            #this will return false if the full name isnt a valid one
+            if bool(re.fullmatch('[A-Za-z]{2,25}( [A-Za-z]{2,25})?', name)):
                 return redirect(url_for('thankyou', name=name))
             else:
                 return redirect(url_for('main_page', bad_input=0))
