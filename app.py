a775a76236bfb5487fbb3b2b12f47c4ad6ae3591
Kevin Tao <kevintao841@yahoo.co.uk>
2021-11-05 11:36:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['POST', 'GET'])
 def main_page():
-    rating = list(range(0,9))
     form_data = request.form
-
     if request.args.get('bad_input') == "0":
         if request.method == 'POST':
             if request.form['customer_name'] != "":
+                # correct inputs
                 name = request.form['customer_name']
                 return redirect(url_for('thankyou', name=name))
             else:
+                #if bad input eg bad name
                 return redirect(url_for('main_page', bad_input=0))
-        # this will never be
+        # this will never be reached 
         return render_template("first_page_alt.html")
 
     else:
@@ -26,8 +26,7 @@ def main_page():
                 return redirect(url_for('thankyou', name=name))
             else:
                 return redirect(url_for('main_page', bad_input=0))
-        return render_template("first_page.html", rating = rating, form_data = form_data)
-
+        return render_template("first_page.html", form_data = form_data)
 
 
 @app.route("/thankyou")
